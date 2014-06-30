201f1ecdb3049a7d663609cc673feb34925df906
cmd545 <me@cmd545.com>
2014-06-30 11:55:51 -0400

    New functions

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-var phrases = Array("It is certain","It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful");
-
-var randomPhrase = phrases[Math.floor(Math.random()*phrases.length)];
-
-var div = document.getElementById("phrasediv");
-
 var askButton = document.getElementById("btn");
 
-var circle = document.getElementById("magicball");
+function switchIMG() {
 
-var triangle = document.getElementById("die");
+	var xmlhttp;
 
-function getTriangle(){
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "pieces.html");
-	xhr.responseType = "document";
-	xhr.send();
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+	  xmlhttp=new XMLHttpRequest();
+	}
+
+	else{ // code for IE6, IE5
+	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	xmlhttp.open("GET","pieces.html",true);
+	xmlhttp.responseType = "document";
+	xmlhttp.send();
+
+	xmlDoc=xmlhttp.responseXML;
+
+	document.body.appendChild(xmlDoc);
+
 }
 
-askButton.onclick = getTriangle;
+askButton.onclick = switchIMG;
