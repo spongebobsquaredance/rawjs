9967445a3ba8f8a40be52f06c41aff5f72672883
cmd545 <me@cmd545.com>
2014-06-30 12:31:59 -0400

    New js file

---
+++ ajax.js
@@ -0,0 +1,32 @@
+var askButton = document.getElementById("btn");
+
+function loadXMLDoc() {
+    var xmlhttp;
+
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 ) {
+           if(xmlhttp.status == 200){
+               document.getElementById("mainpage").innerHTML = xmlhttp.responseText;
+           }
+           else if(xmlhttp.status == 400) {
+              alert('There was an error 400')
+           }
+           else {
+               alert('something else other than 200 was returned')
+           }
+        }
+    }
+
+    xmlhttp.open("GET", "pieces.txt", true);
+    xmlhttp.send();
+}
+
+askButton.onclick = loadXMLDoc;
